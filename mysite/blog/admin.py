c60c1cadf06861e98222630f86245e654800082b
Mario Cortés <marioantonio.cortes@gmail.com>
2023-12-05 22:58:31 -0300

    Fix date_hierarchy value

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
@@ -9,6 +9,6 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'body']
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
-    date_hierarchy = ['publish']
+    date_hierarchy = 'publish'
     ordering = ['status', 'publish']
 
