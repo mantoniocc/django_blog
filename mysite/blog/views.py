b7761d1aee86f594850aebed35089b47aa3b9a9f
Mario Cortés <marioantonio.cortes@gmail.com>
2023-12-20 08:43:05 -0300

    Remove try except block in post_detail view and replace it with get_object_or_404()

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import Post
 from django.http import Http404
 
@@ -7,10 +7,8 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
 
-    return render(request,'blog/post/detail.html', context={'post': post})
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request, 'blog/post/detail.html', context={'post': post})
